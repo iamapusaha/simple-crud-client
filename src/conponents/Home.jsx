c219f0bcc67f3f37c5a60dd1fe188544b774e695
iamapusaha <apusaha392@gmail.com>
2023-10-12 15:37:34 +0600

    ready form for collect data

--- src/conponents/Home.jsx
+++ src/conponents/Home.jsx
@@ -1,9 +1,21 @@
 
 
 const Home = () => {
+    const handleAddUser = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const user = { name, email }
+        console.log(user);
+    }
     return (
         <div>
-            <h1>this is our home</h1>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" /><br />
+                <input type="email" name="email" id="" /><br />
+                <input type="submit" value="Add User" />
+            </form>
         </div>
     );
 };
