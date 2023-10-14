405f16d66988fde04c9c86bf4be0eba6091afef7
iamapusaha <apusaha392@gmail.com>
2023-10-14 14:22:02 +0600

    update data via put method

--- src/conponents/Update.jsx
+++ src/conponents/Update.jsx
@@ -8,7 +8,18 @@ const Update = () => {
         const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
-        console.log(name, email);
+        const user = { name, email }
+        fetch(`http://localhost:5000/users/${loadUsers._id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
     }
     return (
 
