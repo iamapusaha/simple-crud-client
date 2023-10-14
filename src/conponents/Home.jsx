3f1217d546782d0c4f94101eae4f3205d5ced5d5
iamapusaha <apusaha392@gmail.com>
2023-10-14 13:12:57 +0600

    delete user from client site

--- src/conponents/Home.jsx
+++ src/conponents/Home.jsx
@@ -17,12 +17,14 @@ const Home = () => {
         })
             .then(res => res.json())
             .then(data => console.log(data))
+        form.reset();
+        alert('form submited!')
     }
     return (
         <div>
             <form onSubmit={handleAddUser}>
-                <input className="border p-2" placeholder="name" type="text" name="name" /><br />
-                <input className="border p-2" placeholder="email" type="email" name="email" id="" /><br />
+                <input className="border p-2" placeholder="name" type="text" name="name" required /><br />
+                <input className="border p-2" placeholder="email" type="email" name="email" id="" required /><br />
                 <button><input type="submit" value="Add User" /></button>
             </form>
         </div>
--- src/conponents/User.jsx
+++ src/conponents/User.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const User = () => {
-    const users = useLoaderData()
+    const loadUsers = useLoaderData()
+    const [users, setUsers] = useState(loadUsers)
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    const remaing = users.filter(user => user._id !== id)
+                    setUsers(remaing)
+                    alert("Successfully deleted one document.");
+                }
+            })
+    }
     return (
         <div>
             <h1>Total number of user{users.length}</h1>
             {
-                users.map(user => <p key={user._id}>{user.name}</p>)
+                users.map(user => <p key={user._id}>{user.name} <button onClick={() => handleDelete(user._id)}>x</button></p>)
             }
         </div>
     );
